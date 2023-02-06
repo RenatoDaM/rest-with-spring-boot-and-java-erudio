a1deeea1eceb43b0f9e20948bb0dd968b045a95d
Renato <renaato.d.m@gmail.com>
2023-02-06 00:45:47 -0300

    atualizado

--- src/main/java/com/example/restwithspringbootandjavaerudio/config/OpenApiConfig.java
+++ src/main/java/com/example/restwithspringbootandjavaerudio/config/OpenApiConfig.java
@@ -2,7 +2,6 @@ package com.example.restwithspringbootandjavaerudio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-aaaaaaaaaaaaaaaaa
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
