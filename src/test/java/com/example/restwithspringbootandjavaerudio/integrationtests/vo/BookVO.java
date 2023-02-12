426b7b50f6cd85c094860188f08507813380fac0
Renato <renaato.d.m@gmail.com>
2023-02-12 05:56:44 -0300

    implementado teste xml para BookControllerXmlTest

--- src/test/java/com/example/restwithspringbootandjavaerudio/integrationtests/vo/BookVO.java
+++ src/test/java/com/example/restwithspringbootandjavaerudio/integrationtests/vo/BookVO.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @XmlRootElement(name = "BookVO")
 @JsonPropertyOrder({"id", "author", "launchDate", "price", "title"})
-public class BookVO extends RepresentationModel<BookVO> implements Serializable {
+public class BookVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
