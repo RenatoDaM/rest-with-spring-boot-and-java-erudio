631d8a6ed0d3228cf8e17ac67f568a3dadc3f26d
Renato <renaato.d.m@gmail.com>
2023-02-22 03:14:29 -0300

    dockerizando a aplicação

--- src/main/java/com/example/restwithspringbootandjavaerudio/services/FileStorageService.java
+++ src/main/java/com/example/restwithspringbootandjavaerudio/services/FileStorageService.java
@@ -2,7 +2,10 @@ package com.example.restwithspringbootandjavaerudio.services;
 
 import com.example.restwithspringbootandjavaerudio.config.FileStorageConfig;
 import com.example.restwithspringbootandjavaerudio.exceptions.FileStorageException;
+import com.example.restwithspringbootandjavaerudio.exceptions.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,6 +52,17 @@ public class FileStorageService {
         } catch (Exception e) {
             throw new FileStorageException(
                     "Could not store file " + fileName + ". Please try again." , e);
+        }
+    }
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename);
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) return resource;
+            else throw new MyFileNotFoundException("File not found");
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found" + filename, e);
         }
     }
 }
